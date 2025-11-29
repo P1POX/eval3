b5ee08ff56e7e621b63cce31ca6553562a5f7964
P1POX <59937081+P1POX@users.noreply.github.com>
2025-11-29 16:16:21 +0000

    create html

--- web/app.py
+++ web/app.py
@@ -4,13 +4,15 @@ import mysql.connector
 app = Flask(__name__)
 @app.route("/")
 def home():
-    conn = mysql.connector.connect(host="db")
+    conn = mysql.connector.connect(host="db", user="root", password="root")
     cursor=conn.cursor()
     cursor.execute("SELECT 'flask conectado'")
-    result = cursor.fetchome()[0]
+    result = cursor.fetchone()[0]
     cursor.close()
     conn.close()
-    return result
+    return render_template(
+        "index.html"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
